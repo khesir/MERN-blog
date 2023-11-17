d519e57418adfc9f8fa7dc21758e797ba2707701
ajtollo <khesir2002@gmail.com>
2023-11-17 08:55:31 +0800

    Article page added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Homepage from './pages/Homepage';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import CreatePost from './pages/Createpost';
+import ViewPost from './pages/Viewpost';
+import Profile from './pages/Profile';
+import LoginPage from './pages/Login';
+import RegisterPage from './pages/Register';
 
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+const Layout = ()=>{
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/create',
+        element: <CreatePost />
+      },{
+        path: '/post/:id',
+        element: <ViewPost />
+      },{
+        path: '/profile/:id',
+        element: <Profile />
+      }
+    ]
+  },
+  {
+    path: "/login",
+    element: <LoginPage />
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />
+  },
+  {
+    path: "*",
+    element: <h1>Not Found you silly</h1>
+  }
+])
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route index path="/home" element={<Homepage />} />
-      </Routes>
+      <div className="container">
+        <RouterProvider router = {router}></RouterProvider>
+      </div>
     </div>
   );
 }
